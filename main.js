5a84c4b1de1d784e08c4561e6b47ff0d1675fe08
trollface939 <trollface939@yahoo.com.ph>
2022-09-20 23:53:46 +0800

    changes

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+var saveGame = localStorage.getItem('goldMinerSave')
 var gameData = {
     gold: 0,
     goldPerClick: 1,
-    goldPerClickCost: 10
+    goldPerClickCost: 10,
+    lastTick: Date.now()
+}
+
+function update(id, content) {
+  document.getElementById(id).innerHTML = content;
 }
 
 function mineGold() {
@@ -28,7 +34,10 @@ function buyGoldPerClick() {
 }
 
 var mainGameLoop = window.setInterval(function() {
-    mineGold()
+  diff = Date.now() - gameData.lastTick;
+  gameData.lastTick = Date.now() // Don't forget to update lastTick.
+  gameData.gold += gameData.goldPerClick * (diff / 1000) // divide diff by how often (ms) mainGameLoop is ran
+  document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
 }, 1000)
 
 var saveGameLoop = window.setInterval(function() {
@@ -40,4 +49,24 @@ if (savegame !== null) {
   gameData = savegame
 }
 
-if (typeof savegame.dwarves !== "undefined") gameData.dwarves = savegame.dwarves;
+function format(number, type) {
+	let exponent = Math.floor(Math.log10(number))
+	let mantissa = number / Math.pow(10, exponent)
+	if (exponent < 3) return number.toFixed(1)
+	if (type == "scientific") return mantissa.toFixed(2) + "e" + exponent
+	if (type == "engineering") return (Math.pow(10, exponent % 3) * mantissa).toFixed(2) + "e" + (Math.floor(exponent / 3) * 3)
+}
+
+function tab(tab) {
+  // hide all your tabs, then show the one the user selected.
+  document.getElementById("mineGoldMenu").style.display = "none"
+  document.getElementById("shopMenu").style.display = "none"
+  document.getElementById(tab).style.display = "inline-block"
+}
+// go to a tab for the first time, so not all show
+tab("mineGoldMenu")
+
+if (typeof saveGame.gold !== "undefined") gameData.gold = saveGame.gold;
+if (typeof saveGame.goldPerClick !== "undefined") gameData.goldPerClick = saveGame.goldPerClick;
+if (typeof saveGame.goldPerClickCost !== "undefined") gameData.goldPerClickCost = saveGame.goldPerClickCost;
+if (typeof saveGame.lastTick !== "undefined") gameData.lastTick = saveGame.lastTick;
